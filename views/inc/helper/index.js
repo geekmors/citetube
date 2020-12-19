40b1db1932a5fd7aafb61d89aafc86decec46911
geekmors <info@codingparrot.com>
2020-12-19 15:38:15 -0600

    added vue prod mode

--- views/inc/helper/index.js
+++ views/inc/helper/index.js
@@ -1,6 +1,12 @@
 const Env = require('../../../src/lib/Env')
 module.exports = {
     app: {
-        name: Env.get('APP_NAME', 'Cite Tube')
+        name: Env.get('APP_NAME', 'Cite Tube'),
+    },
+    Vue:()=>{
+        return (Env.get('PROD', false)
+            ?'<script src="https://cdn.jsdelivr.net/npm/vue@2"></script>'
+            :'<script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>'
+        )
     }
 }
